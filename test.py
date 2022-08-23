dd5d60d4546281d8b672d51ab114355569cb7349
Suraj Rimal <surajrimal50@gmail.com>
2022-08-23 15:28:13 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print("hello")
+import pandas as pd
+import json 
+
+with open('yourfile.json') as json_file:
+    data = json.load(json_file)
+
+df= pd.json_normalize(data,max_level=1)
+#max_level is for nested json
+print(df)
